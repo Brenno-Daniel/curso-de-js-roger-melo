064c34bcae8ce15eb3e09ee1862dd42132a7ca78
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-05-29 19:06:08 -0300

    functions vs methods

---
+++ Etapa03-Funcoes-e-Metodos/aula2/2-funcoes_vs_metodos.js
@@ -0,0 +1,11 @@
+const name = 'Brenno';
+
+// funções
+const sayHi = () => 'Oiii';
+
+const greet = sayHi();
+console.log(greet);
+
+// métodos
+const nameInUpperCase = name.toUpperCase();
+console.log(nameInUpperCase);
