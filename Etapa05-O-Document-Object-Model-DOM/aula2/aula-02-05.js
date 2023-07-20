d2ff70220abf12db6f7676ca8a81d7e5bd7ba916
Brenno Daniel <brenno.daniel.98@gmail.com>
2023-07-19 21:58:04 -0300

    update class 02: - change file names; - added more lessons of class 02; - added exercise 14 (TO DO exercises);

---
+++ Etapa05-O-Document-Object-Model-DOM/aula2/aula-02-05.js
@@ -0,0 +1,33 @@
+// const paragraph = document.querySelector('p');
+
+// // ver lista de classes do elemento
+// console.log(paragraph.classList);
+
+// // adicionar classe para um elemento html
+// paragraph.classList.add('error');
+
+// // remover classe
+// paragraph.classList.remove('error');
+
+// paragraph.classList.add('success');
+
+// adicionar classe error onde tem palavra error e classe success onde tem palavra success:
+const paragraphs = document.querySelectorAll('p');
+
+// innerText vai receber todo texto visível que o elemento contém
+// textContent vai receber todo texto que o elemento contém independente de estar visível ou não
+paragraphs.forEach(paragraph => {
+    if (paragraph.textContent.includes('error')) {
+        paragraph.classList.add('error');
+    }
+
+    if (paragraph.textContent.includes('success')) {
+        paragraph.classList.add('success');
+    }
+});
+
+const title = document.querySelector('h1');
+
+// método toggle busca a classe, caso não tenha a classe o toggle adiciona, caso já exista a classe então ele remove a classe
+title.classList.toggle('test'); // como não existe a classe test, ele adiciona a mesma
+title.classList.toggle('test'); // como já existe a classe test, ele remove a mesma
