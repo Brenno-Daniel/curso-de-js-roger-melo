932548e1af8e157166ad7ae57ff5917423f07d40
Brenno Daniel <brenno.daniel.98@gmail.com>
2024-05-09 21:01:15 -0300

    finish exercise and made some refacs

--- Etapa05-O-Document-Object-Model-DOM/aula4/exercicio-16/app.js
+++ Etapa05-O-Document-Object-Model-DOM/aula4/exercicio-16/app.js
@@ -10,21 +10,27 @@ const elementsInsideDiv = Array.from(div.children);
 let clickedChildDivMessage = '';
 let clickedDivMessage = '';
 
-elementsInsideDiv.forEach(element => {
-    element.addEventListener('click', event => {
-        const tagName = event.target.tagName.toUpperCase();
+const setClickEventListenerToElementAndTextToH2 = element => {
+    element.addEventListener('click', event => setClickedChildDivWithTagNameUppercaseTextToH2(event));
+};
 
-        // console.log(`Clicou no ${tagName}, filho da div.`);
-        h2.innerText = `Clicou no ${tagName}, filho da div.`;
-    });
-});
+const setClickedChildDivWithTagNameUppercaseTextToH2 = event => {
+    const tagName = event.target.tagName.toUpperCase();
 
-div.addEventListener('click', event => {
-    if (event.target.textContent !== 'TV portátil') {
-        // console.log('Clicou na div.');
+    h2.innerText = `Clicou no ${tagName}, filho da div.`;
+};
+
+elementsInsideDiv.forEach(element => setClickEventListenerToElementAndTextToH2(element));
+
+div.addEventListener('click', event => setClickedDivTextToH2WhenIsPortableTV(event));
+
+const setClickedDivTextToH2WhenIsPortableTV = event => {
+    const isPortableTV = event.target.textContent !== 'TV portátil';
+
+    if (isPortableTV) {
         h2.innerText = `${h2.innerText} \n Clicou na div.`;
     }
-});
+};
 
 /*
   02
@@ -43,7 +49,7 @@ div.addEventListener('click', event => {
 */
 
 const h2 = document.createElement('h2');
-document.querySelector('div').append(h2);
+document.querySelector('div').insertAdjacentElement('afterend', h2);
 
 /*
   04
@@ -64,12 +70,28 @@ h2.addEventListener('copy', () => {
     "Eixo X: COORDENADA_EIXO_X | Eixo Y: COORDENADA_EIXO_Y".
 */
 
+const eggDiv = document.querySelector('.egg');
+
+const setOffsetXAndYToEggDivContent = event => {
+    eggDiv.textContent = `X: ${event.offsetX} | Y: ${event.offsetY}`;
+};
+
+eggDiv.addEventListener('mousemove', event => setOffsetXAndYToEggDivContent(event));
+
 /*
   06
 
   - Modifique a cor do ovo para "lightgoldenrodyellow" quando o botão for 
     clicado.
 */
+
+const changeEggColorButton = document.querySelector('button');
+
+const setBackgroundColorToEggDiv = () => {
+    eggDiv.style.background = 'lightgoldenrodyellow';
+};
+
+changeEggColorButton.addEventListener('click', () => setBackgroundColorToEggDiv());
 
 /*
   07
@@ -91,3 +113,13 @@ const people = [
     { id: 8, name: 'Matheus Manucci', profession: 'Piloto' },
     { id: 9, name: 'Hamilton Silva', profession: 'Advogado' },
 ];
+
+const logMessageIfContainSomeFrontendDeveloper = person => {
+    const isFrontendDeveloperProfession = person.profession === 'Front-end developer';
+
+    if (isFrontendDeveloperProfession) {
+        console.log('O array people contém, no mínimo, um(a) Front-end developer.');
+    }
+};
+
+people.some(person => logMessageIfContainSomeFrontendDeveloper(person));
